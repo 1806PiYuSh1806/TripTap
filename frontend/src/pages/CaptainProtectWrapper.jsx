c3b844b9a72f6746bfa83ecc8485bd984bc9f8d2
1806PiYuSh1806 <piyushupadhyay37756@gmail.com>
2025-03-06 16:17:34 +0530

    captain ui completed

--- frontend/src/pages/CaptainProtectWrapper.jsx
+++ frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { CaptainDataContext } from '../context/CaptainContext'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
@@ -19,23 +19,25 @@ const CaptainProtectWrapper = ({
         if (!token) {
             navigate('/captain-login')
         }
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                setCaptain(response.data.captain)
+                setIsLoading(false)
+            }
+        })
+            .catch(err => {
+                console.log(children)
+                localStorage.removeItem('token')
+                navigate('/captain-login')
+            })
     }, [ token ])
 
-    axios.get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }).then(response => {
-        if (response.status === 200) {
-            setCaptain(response.data.captain)
-            setIsLoading(false)
-        }
-    })
-        .catch(err => {
-            console.log(err)
-            localStorage.removeItem('token')
-            navigate('/captain-login')
-        })
+    
 
     if (isLoading) {
         return (
